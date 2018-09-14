afbe35f6d020fe0873a660a7471e479638604ecb
johari2020 <42872004+johari2020@users.noreply.github.com>
2018-09-14 12:01:01 -0400

    Updated from Brython Server: 9/14/2018 12:01:00 PM

--- birthday.py
+++ birthday.py
@@ -1,7 +1,7 @@
 """
 birthday.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Johari
+Credit: megsnyder, noah 
 Assignment:
 
 Your program will ask the user the following questions, in this order:
@@ -35,6 +35,8 @@ from datetime import datetime
 from calendar import month_name
 todaymonth = datetime.today().month
 todaydate = datetime.today().day
+mounth = month_name[todaymonth]
+
 
 name = input("Hello, what is your name? ")
 month = input("Hi " + name + ", what was the name of the month you were born in?")
@@ -45,17 +47,58 @@ year = int(y)
 
 
 if month == "October" and day == 31:
-    print("You were born on Halloween!") 
+    print("You were born on Halloween!")
+    
 elif month == todaymonth and day == todaydate:
     print("Happy birthday!") 
-elif month == "December" or "January" or "February":
-    print ("You are a winter baby")
-elif month == "March" or "April" or "May": 
-    print("You are a spring baby")
-elif month == "June" or "July" or "August":
-    print("You are a summer baby") 
-elif month == "September" or "October" or "November":
-    print("You are a fall baby") 
+    
+elif month == "December" or month == "January" or month == "February" and year >= 1990 and year <= 1999:
+    print ("You are a winter baby of the nineties. ")
+    
+elif month == "March" or month == "April" or month == "May" and year >= 1990 and year <= 1999 : 
+    print("You are a spring baby of the nineties.")
+    
+elif month == "June" or month == "July" or month == "August" and year >= 1990 and year <= 1999:
+    print("You are a summer baby of the nineties.") 
+    
+elif month == "September" or month == "October" or month == "November" and year >= 1990 and year <= 1999:
+    print("You are a fall baby of the nineties.") 
+
+elif month == "December" or month == "January" or month == "February" and year >= 1980 and year <= 1989:
+    print ("You are a winter baby of the eighties.")
+    
+elif month == "March" or month == "April" or month == "May" and year >= 1980 and year <= 1989 : 
+    print("You are a spring baby of the eighties.")
+    
+elif month == "June" or month == "July" or month == "August" and year >= 1980 and year <= 1989:
+    print("You are a summer baby of the eighties.") 
+    
+elif month == "September" or month == "October" or month == "November" and year >= 1980 and year <= 1989:
+    print("You are a fall baby of the eighties.") 
+
+elif month == "December" or month == "January" or month == "February" and year >= 2000:
+    print ("You are a winter baby of the two thousands.")
+    
+elif month == "March" or month == "April" or month == "May" and year >= 2000: 
+    print("You are a spring baby of the two thousands.")
+    
+elif month == "June" or month == "July" or month == "August" and year >= 2000:
+    print("You are a summer baby of the two thousands.") 
+    
+elif month == "September" or month == "October" or month == "November" and year >= 2000:
+    print("You are a fall baby of the two thousands.") 
+    
+elif month == "December" or month == "January" or month == "February" and year < 1980:
+    print ("You are a winter baby of the stone age.")
+    
+elif month == "March" or month == "April" or month == "May" and year < 1980: 
+    print("You are a spring baby of the stone age.")
+    
+elif month == "June" or month == "July" or month == "August" and year < 1980:
+    print("You are a summer baby of the stone age.") 
+    
+elif month == "September" or month == "October" or month == "November" and year < 1980:
+    print("You are a fall baby of the stone age.") 
 else:
     print("ok")
 
